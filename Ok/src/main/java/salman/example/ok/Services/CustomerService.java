f132b0e204140f4783bd66b48fcde53cc0265602
Crowned-Eagle-1 <salmanfarshe300701@gmail.com>
2026-02-06 17:06:19 +0600

    CRUD cover in Customer tier

--- Ok/src/main/java/salman/example/ok/Services/CustomerService.java
+++ Ok/src/main/java/salman/example/ok/Services/CustomerService.java
@@ -1,4 +1,34 @@
 package salman.example.ok.Services;
 
+import org.springframework.stereotype.Service;
+import salman.example.ok.Models.Customer;
+import salman.example.ok.Models.Product;
+import salman.example.ok.Repository.CustomerRepo;
+import salman.example.ok.Repository.ProductRepo;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+    CustomerRepo repo;
+
+    public CustomerService(CustomerRepo repo)
+    {
+        this.repo=repo;
+    }
+
+    public Customer save(Customer customer)
+    {
+        return repo.save(customer);
+    }
+
+    public List<Customer> getAll()
+    {
+        return repo.findAll();
+    }
+
+    public Customer getById(Integer id)
+    {
+        return repo.findById(id).orElse(null);
+    }
 }
