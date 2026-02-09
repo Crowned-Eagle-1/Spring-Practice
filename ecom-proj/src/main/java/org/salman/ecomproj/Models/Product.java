dbf58de332731bdf1c266647e53bf6047ec0cdb2
Farshe-Salman <salmanfarshe3071@gmail.com>
2026-02-09 09:40:39 +0600

    Add com proj

--- ecom-proj/src/main/java/org/salman/ecomproj/Models/Product.java
+++ ecom-proj/src/main/java/org/salman/ecomproj/Models/Product.java
@@ -14,11 +14,82 @@ public class Product {
     private String name;
     private String description;
     private String brand;
-    private BigDecimal Price;
+    private BigDecimal price;
     private String category;
     private Date relaseDate;
     private Boolean available;
     private int quantity;
 
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getRelaseDate() {
+        return relaseDate;
+    }
+
+    public void setRelaseDate(Date relaseDate) {
+        this.relaseDate = relaseDate;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
--- ecom-proj/src/main/java/org/salman/ecomproj/Services/ProductService.java
+++ ecom-proj/src/main/java/org/salman/ecomproj/Services/ProductService.java
@@ -1,12 +1,22 @@
 package org.salman.ecomproj.Services;
 
+import org.salman.ecomproj.Models.Product;
 import org.salman.ecomproj.Repos.ProductRepo;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class ProductService {
     ProductRepo repo;
 
     public ProductService(ProductRepo repo)
     {
         this.repo=repo;
+    }
+
+    public List<Product> GetAllProducts()
+    {
+        return repo.findAll();
     }
 }
