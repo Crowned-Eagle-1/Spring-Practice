ad85df2bf4c0cfe02a6ad5a45a45fc57009dc2bb
Farshe-Salman <salmanfarshe3071@gmail.com>
2026-02-09 09:45:02 +0600

    ok

---
+++ ecom-proj/target/classes/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO products
+(name, description, brand, price, category, relase_date, available, quantity)
+VALUES
+    ('iPhone 15', 'Latest Apple phone', 'Apple', 1200.00, 'Electronics', CURRENT_DATE, true, 10),
+    ('Galaxy S24', 'Flagship Samsung phone', 'Samsung', 999.99, 'Electronics', CURRENT_DATE, true, 15),
+    ('Pixel 8', 'Google AI powered phone', 'Google', 899.50, 'Electronics', CURRENT_DATE, true, 12),
+    ('OnePlus 12', 'Fast and smooth performance', 'OnePlus', 799.00, 'Electronics', CURRENT_DATE, true, 20),
+    ('MacBook Air M3', 'Lightweight Apple laptop', 'Apple', 1500.00, 'Computers', CURRENT_DATE, true, 5);
+--     ('Dell XPS 15', 'Premium Windows laptop', 'Dell', 1800.00, 'Computers', CURRENT_DATE, true, 7);
